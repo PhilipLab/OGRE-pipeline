efb92e7c009e2c9d9fd528d30e96362841370899
Benjamin Philip <bphilip@wustl.edu>
2024-07-01 16:57:49 -0500

    makeregdir updated functionality

--- lib/OGREmakeregdir.sh
+++ lib/OGREmakeregdir.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-STUDYPATH=/Users/Shared/10_Connectivity
 T1=1
 ATLAS=2
+OVERWRITE=0
 
 #START240417
 #Hard coded freesurfer version options: 5.3.0-HCP 7.2.0 7.3.2 7.4.0 7.4.1
@@ -10,16 +10,18 @@ ATLAS=2
 
 helpmsg(){
     echo "Builds reg directory within a .feat folder, from OGRE results."
+    echo "Defaults will work correctly if your .feat folder is in STUDY/derivatives/analysis/sub-X and your OGRE output is in STUDY/derivatives/preprocessed/sub-X"
     echo "Required: ${root0} <Feat directory>:"
-    echo "    -f --feat -feat       Feat directory. If no -option provided, then input is assumed to be this."
-    echo "                          E.g. /Users/Shared/10_Connectivity/derivatives/analysis/sub-1001/sub-1001_model-OGRE-7.4.1/sub-1001_RHflip_susan-6_run-1.feat/"
-    echo "                          If input doesn't end in .feat, will treat it as relative path in current directory (e.g. sub-1001_RHflip_susan-6_run-1)"
+    echo "    -f --feat -feat       Feat directory. If no -argument provided, then input is assumed to be this."
+    echo "                          E.g. /Users/Shared/10_Connectivity/derivatives/analysis/sub-1001/sub-1001_model-OGRE-7.4.1/sub-1008_task-drawLH_run-1.feat"
+    echo "                          If you want the script to look for a path in current directory (rather than a full path), omit the final .feat"
     echo " "
-    echo "  Everything below here is OPTIONAL. Defaults to BIDS based on sub/study info"
-    echo "    -s --sub -sub         Subject name, e.g. 2000, is read from the FEAT directory. This option will override that of the FEAT directory." 
+    echo "  Everything below here is OPTIONAL. Defaults to pulling from your feat directory path & assuming it is in BIDS "
+    echo "    -v --overwrite        Flag. If set, overwrite existing makeregdir output. Otherwise, if makeregdir output exists, script does nothing."
+    echo "    -s --sub -sub         Subject name, e.g. sub-2000. This option will override that of the FEAT directory." 
     echo "                          Used to find anatomical and other data in BIDS"
-    echo "    -y -S --study -study  OPTIONAL. Study path. Default is /Users/Shared/10_Connectivity" 
-    echo "                          Syntax: if full path is /Users/Shared/10_Connectivity/10_2000/pipeline7.4.0, STUDYPATH is /Users/Shared/10_Connectivity"  
+    echo "    -o --ogredir          Specify location of OGRE subject directory (i.e. dir that contains subdirectories pipelineVVV, func, anat...)"
+
     echo " "
 
     #START240417
@@ -37,14 +39,14 @@ helpmsg(){
     echo "                                NOTE: MNINonLinear/T1w_restore.2 is not the correct image. It is a so-called subcortical T1 for surface analysis."
     echo "    --t1highreshead -t1highreshead --t1hireshead -t1hireshead"
     echo "                            Input your own whole head T1."
-    echo "                            Ex. --t1highreshead /Users/Shared/10_Connectivity/sub-1001/pipelineT7.4.1/func/sub-1001_OGRE-preproc_desc-restore_T1w.nii.gz"
+    echo "                            Ex. --t1highreshead /Users/Shared/10_Connectivity/sub-1001/pipeline7.4.1/func/sub-1001_OGRE-preproc_desc-restore_T1w.nii.gz"
     echo "    --t1highres -t1highres --t1hires -t1hires"
     echo "                            Input your own brain masked T1."
     echo "                            Ex. --t1highres /Users/Shared/10_Connectivity/10_1001/pipelineTest7.4.0/MNINonLinear/T1w_restore_brain.nii.gz"
     echo ""
     echo "    -u --atlas -atlas       Standard image resolution (ie FEAT standard). 1 or 2. Default is 2mm."
-    echo "                            If 1mm, then ${FSLDIR}/data/standard/MNI152_T1_1mm and ${FSLDIR}/data/standard/MNI152_T1_1mm_brain are used."
-    echo "                            If 2mm, then ${FSLDIR}/data/standard/MNI152_T1_2mm and ${FSLDIR}/data/standard/MNI152_T1_2mm_brain are used."
+    echo "                            If 1mm, then FSLDIR/data/standard/MNI152_T1_1mm and FSLDIR/data/standard/MNI152_T1_1mm_brain are used."
+    echo "                            If 2mm, then FSLDIR/data/standard/MNI152_T1_2mm and FSLDIR/data/standard/MNI152_T1_2mm_brain are used."
     echo "    --standardhead -standardhead"
     echo "                            Input your own whole head standard image."
     echo "                            Ex. --standardhead /Users/Shared/10_Connectivity/sub-1001/pipelineTest7.4.0/MNINonLinear/T1w_restore.nii.gz"
@@ -76,14 +78,12 @@ for((i=0;i<${#@};++i));do
             #START231219
             #((idx+=2))
             ;;
-
-        -S | -y | --study | -study)
-            STUDYPATH=${arg[((++i))]}
-            echo "STUDYPATH=$STUDYPATH"
-            #START231218
-            #((narg-=2))
+        -v | --v | --overwrite | -overwrite)
+            OVERWRITE=1
             ;;
-
+        -o | --o | --ogredir | -ogredir)
+            OGRESUBDIR=${arg[((++i))]}
+            ;;
         #START240417
         -V | --VERSION | -VERSION | --FREESURFVER | -FREESURFVER | --freesurferVersion | -freesurferVersion)
             FREESURFVER=${arg[((++i))]}
@@ -145,50 +145,58 @@ fi
 
 [ -n "${unexpected}" ] && FEATDIR+=(${unexpected[@]})
 if [ -z "${FEATDIR}" ];then
-    echo "Need to provide FEATDIR"
+    echo "Error in OGREmakeregdir.sh: must provide FEATDIR"
     exit
 fi
 
 # Ben update 240521 to handle either full path or relative file
-if [[ $FEATDIR == *".feat" ]];then # if ends in .feat, reduces it to relative file
+if [[ $FEATDIR == *".feat" ]];then # if ends in .feat, reduces it to relative file 
+    FEATDIR_LONG=${FEATDIR}
     DIRTEMP=${FEATDIR##*/} #everything after the last /
-    FEATDIR=${DIRTEMP%%.*} #everthing before the last .
+    FEATDIR_SHORT=${DIRTEMP%%.*} #everthing before the last .
+else # if a relative path, then get info from current directory location (which should be a subject dir)
+    CURDIR=`pwd`
+    DERIVDIR="${CURDIR%%derivatives*}derivatives"
+    TEMPSUBJECT=${CURDIR##*/} #everything after the last /
+    SUBJECT=${TEMPSUBJECT%%_*} #everthing before the first _
+    FEATDIR_LONG=${DERIVDIR}/analysis/${SUBJECT}/${SUBJECT}_model-OGRE/${FEATDIR}.feat
+    FEATDIR_SHORT=${FEATDIR}
 fi
 
-echo "FEATDIR=$FEATDIR"
+#echo "FEATDIR_LONG=$FEATDIR_LONG"
+#echo "FEATDIR_SHORT=$FEATDIR_SHORT"
 
 if [ -z "${SUBJECT}" ];then
     #SUBJECT=$(awk -F'/sub-' '{print $2}' <<< "$FEATDIR")
-    #START240519
-    SUBJECT=${FEATDIR##*/} #everything after the last /
-    SUBJECT=${SUBJECT%%_*} #everthing before the first _
+    TEMPSUBJECT=${FEATDIR_SHORT##*/} #everything after the last /
+    SUBJECT=${TEMPSUBJECT%%_*} #everthing before the first _
 fi
 echo "SUBJECT=$SUBJECT"
 
+if [ -z "${OGRESUBDIR}" ]; then
+    STUDYPATH=${FEATDIR_LONG%%/derivatives*}
+    DERIVDIR="${FEATDIR_LONG%%derivatives*}derivatives"
+    OGRESUBDIR=${DERIVDIR}/preprocessed/${SUBJECT}
+fi
 
-#OUTDIR=${FEATDIR}/reg
-#ANATDIR=${STUDYPATH}/derivatives/preprocessed/sub-${SUBJECT}/anat
-#MNLDIR=${STUDYPATH}/derivatives/preprocessed/sub-${SUBJECT}/pipeline${FREESURFVER}/MNINonLinear
-#START240519
-ANATDIR=${STUDYPATH}/derivatives/preprocessed/${SUBJECT}/anat
-MNLDIR=${STUDYPATH}/derivatives/preprocessed/${SUBJECT}/pipeline${FREESURFVER}/MNINonLinear
-FEATPATH=${STUDYPATH}/derivatives/analysis/${SUBJECT}/${SUBJECT}_model-OGRE/${FEATDIR}.feat
-OUTDIR=${FEATPATH}/reg
+ANATDIR=${OGRESUBDIR}/anat
+MNLDIR=${OGRESUBDIR}/pipeline${FREESURFVER}/MNINonLinear
+OUTDIR=${FEATDIR_LONG}/reg
 
 echo "OUTDIR=$OUTDIR"
 
+MRDFILE="${OUTDIR}/makeregdir.txt"
+if [ -f $MRDFILE ];then
+    echo "Makeregdir output already exists; aborted."
+    exit
+fi
+
+exit
+#echo $DERIVDIR
 
 if [ -z "${T1HIGHRESHEAD}" ];then
     if((T1==1));then
 
-        #if [ -f ${ANATDIR}/sub-${SUBJECT}_OGRE-preproc_desc-restore_T1w.nii.gz ]; then
-        #    T1HIGHRESHEAD=${ANATDIR}/sub-${SUBJECT}_OGRE-preproc_desc-restore_T1w.nii.gz
-        #elif [ -f ${MNLDIR}/T1w_restore.nii.gz ]; then
-        #    T1HIGHRESHEAD=${MNLDIR}/T1w_restore.nii.gz
-        #else
-        #    echo "T1 head 1mm not found: ${T1}"
-        #    exit
-        #fi
         #START240519
         t1bids=${ANATDIR}/${SUBJECT}_OGRE-preproc_desc-restore_T1w.nii.gz
         t1ogre=${MNLDIR}/T1w_restore.nii.gz
@@ -219,14 +227,6 @@ fi
 if [ -z "${T1HIGHRES}" ];then
     if((T1==1));then
 
-        #if [ -f ${ANATDIR}/sub-${SUBJECT}_OGRE-preproc_desc-restore_T1w_brain.nii.gz ]; then
-        #    T1HIGHRES=${ANATDIR}/sub-${SUBJECT}_OGRE-preproc_desc-restore_T1w_brain.nii.gz
-        #elif [ -f ${MNLDIR}/T1w_restore_brain.nii.gz ]; then
-        #    T1HIGHRES=${MNLDIR}/T1w_restore_brain.nii.gz
-        #else
-        #    echo "T1 brain 1mm not found: ${T1}"
-        #    exit
-        #fi
         #START240519
         t1bids=${ANATDIR}/${SUBJECT}_OGRE-preproc_desc-restore_T1w_brain.nii.gz
         t1ogre=${MNLDIR}/T1w_restore_brain.nii.gz
@@ -278,7 +278,7 @@ fi
 #REGSTD=${SUBJDIR}/model/${ANALYSISNAME}.feat/reg
 THEDATE=`date +%y%m%d_%H%M`
 if [[ -d "$OUTDIR" ]];then
-    echo "Reg already found in ${FEATDIR}. Backing up old ${OUTDIR} as ${THEDATE}"
+    echo "Reg already found in ${FEATDIR_LONG}. Backing up old ${OUTDIR} as ${THEDATE}"
     mv ${OUTDIR} ${OUTDIR}_${THEDATE}
     #mkdir -p ${OUTDIR}_${THEDATE}
     #mv ${OUTDIR}/ ${OUTDIR}_${THEDATE}/
@@ -298,11 +298,11 @@ mkdir -p ${OUTDIR}
 #fi
 #cp -p ${FEATDIR}/example_func.nii.gz ${OUTDIR}/example_func.nii.gz
 #START240519
-if [ ! -f ${FEATPATH}/example_func.nii.gz ];then
-    echo "ERROR: ${FEATPATH}/example_func.nii.gz does not exist. Abort!"
+if [ ! -f ${FEATDIR_LONG}/example_func.nii.gz ];then
+    echo "ERROR: ${FEATDIR_LONG}/example_func.nii.gz does not exist. Abort!"
     exit
 fi
-cp -p ${FEATPATH}/example_func.nii.gz ${OUTDIR}/example_func.nii.gz
+cp -p ${FEATDIR_LONG}/example_func.nii.gz ${OUTDIR}/example_func.nii.gz
 
 
 
@@ -313,7 +313,7 @@ cp -p $T1HIGHRESHEAD ${OUTDIR}/highres_head.nii.gz # this is still broken under 
 cp -p $T1HIGHRES ${OUTDIR}/highres.nii.gz 
 
 # make HR2STD
-echo "Starting transformations for "${FEATDIR}
+echo "Starting transformations for "${FEATDIR_LONG}
 
 ${FSLDIR}/bin/flirt -in ${OUTDIR}/highres.nii.gz -ref ${OUTDIR}/standard.nii.gz -out ${OUTDIR}/highres2standard.nii.gz -omat ${OUTDIR}/highres2standard.mat -cost corratio -dof 12 -searchrx -90 90 -searchry -90 90 -searchrz -90 90 -interp trilinear
 
@@ -321,5 +321,7 @@ ${FSLDIR}/bin/flirt -in ${OUTDIR}/highres.nii.gz -ref ${OUTDIR}/standard.nii.gz 
 ${FSLDIR}/bin/epi_reg --epi=${OUTDIR}/example_func --t1=${OUTDIR}/highres_head --t1brain=${OUTDIR}/highres --out=${OUTDIR}/example_func2highres
 # make EF2S
 ${FSLDIR}/bin/convert_xfm -omat ${OUTDIR}/example_func2standard.mat -concat ${OUTDIR}/highres2standard.mat ${OUTDIR}/example_func2highres.mat
-echo "Registration complete for "${FEATDIR}
+echo "Registration complete for "${FEATDIR_LONG}
 
+echo "Registration folder created by OGREmakeregdir.sh on ${THEDATE}" > ${MRDFILE}
+chmod 775 ${MRDFILE}
