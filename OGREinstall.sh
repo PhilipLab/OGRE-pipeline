a773bc0b9a60b62628f1545e79b09a6c0c87301b
Mark McAvoy <mcavoy@wustl.edu>
2024-12-27 19:36:18 -0600

    commit

--- OGREinstall.sh
+++ OGREinstall.sh
@@ -1,23 +1,15 @@
 #!/usr/bin/env bash
 
-#START240420
-#REPO=/Users/mcavoy/repo/OGRE-pipeline/One-Step-General-Registration-and-Extraction-OGRE-pipline
-
 root0=${0##*/}
 helpmsg(){
 
-    #echo "Required: ${root0} <OGRE directory> -v <version number>"
-    #echo "    -i --install -install"
-    #echo "        OGRE will be installed at this location as <install>/<version>."
-    #echo "        An argument without an option is assumed to be the OGRE directory."
-    #START240420
+    echo "Copy scripts and lib directories from local OGRE-pipeline repository for testing."
     echo "Required: ${root0} -r <repository directory> -v <version number>"
     echo "    -r --repo -repo --repository -repository"
     echo "        Repository directory. All code for the install will be pulled from this directory."
     echo "    -i --install -install"
     echo "        OGRE will be installed at this location as <install>/<version>. Default is the working directory."
     echo "        An argument without an option is assumed to be the install directory."
-
     echo "    -v --version -version"
     echo "        Version number."
     echo "    -h --help -help"
@@ -63,11 +55,6 @@ done
 
 [ -n "${unexpected}" ] && install=(${unexpected[@]})
 
-#if [ -z "${install}" ];then
-#    echo "install not set. Abort!"
-#    exit
-#fi
-#START240420
 if [ -z "${version}" ];then
     echo "-version not set. Abort!"
     exit
@@ -99,59 +86,61 @@ if [ -d "$id" ];then
     rm -rf $id
 fi
 
+
+
+#mkdir -p $id/scripts
+#files=(OGREcleanSETUP.sh \
+#       OGREfMRIpipeSETUP.py \
+#       OGREstructpipeSETUP.sh \
+#       OGREdcm2niix.sh \
+#       pdf2scanlist.py)
+#for i in ${files[@]};do
+#    cp -p $repo/scripts/$i $id/scripts
+#done
+#mkdir -p $id/lib
+#files=(OGREAtlasRegistrationToMNI152_FLIRTandFNIRT.sh \
+#       OGREBiasFieldCorrection_sqrtT1wXT1w.sh \
+#       OGRECreateMyelinMaps.sh \
+#       OGREDistortionCorrectionAndEPIToT1wReg_FLIRTBBRAndFreeSurferBBRbased.sh \
+#       OGREFreeSurfer2CaretConvertAndRegisterNonlinear.sh \
+#       OGREFreeSurferHiresPial.sh \
+#       OGREFreeSurferHiresWhite.sh \
+#       OGREFreeSurferPipeline.sh \
+#       OGREFreeSurferPipelineBatch.sh \
+#       OGREGenericfMRIVolumeProcessingPipeline.sh \
+#       OGREGenericfMRIVolumeProcessingPipelineBatch.sh \
+#       OGREIntensityNormalization.sh \
+#       OGREMotionCorrection.sh \
+#       OGREOneStepResampling.sh \
+#       OGREPostFreeSurferPipeline.sh \
+#       OGREPostFreeSurferPipelineBatch.sh \
+#       OGREPreFreeSurferPipeline.sh \
+#       OGREPreFreeSurferPipelineBatch.sh \
+#       OGRESetUpHCPPipeline.sh \
+#       OGRESmoothingProcess.sh \
+#       OGRET1w_restore.sh \
+#       OGRET2wToT1wReg.sh \
+#       OGREmcflirt.sh \
+#       OGREmakeregdir.sh)
+#for i in ${files[@]};do
+#    cp -p $repo/lib/$i $id/lib
+#done
+#START241226
 #mkdir -p $id
-#START240420
-mkdir -p $id/scripts
+#cp -r -p $repo $id
 
-files=(OGREcleanSETUP.sh \
-       OGREfMRIpipeSETUP.py \
-       OGREstructpipeSETUP.sh \
-       OGREdcm2niix.sh \
-       pdf2scanlist.py)
+#echo "repo=$repo"
+#for i in $repo/*;do
+#    echo "i=$i"
+#done   
 
-#for i in ${files[@]};do
-#    cp -p $REPO/$i $id
-#done
-#START240420
-for i in ${files[@]};do
-    cp -p $repo/scripts/$i $id/scripts
-done
+mkdir -p $id
+echo "Copying $repo/scripts to $id"
+cp -Rp $repo/scripts $id
+echo "Copying $repo/lib to $id"
+cp -Rp $repo/lib $id
 
-#mkdir -p $id/HCP/scripts
-#START240420
-mkdir -p $id/lib
 
-files=(OGREAtlasRegistrationToMNI152_FLIRTandFNIRT.sh \
-       OGREBiasFieldCorrection_sqrtT1wXT1w.sh \
-       OGRECreateMyelinMaps.sh \
-       OGREDistortionCorrectionAndEPIToT1wReg_FLIRTBBRAndFreeSurferBBRbased.sh \
-       OGREFreeSurfer2CaretConvertAndRegisterNonlinear.sh \
-       OGREFreeSurferHiresPial.sh \
-       OGREFreeSurferHiresWhite.sh \
-       OGREFreeSurferPipeline.sh \
-       OGREFreeSurferPipelineBatch.sh \
-       OGREGenericfMRIVolumeProcessingPipeline.sh \
-       OGREGenericfMRIVolumeProcessingPipelineBatch.sh \
-       OGREIntensityNormalization.sh \
-       OGREMotionCorrection.sh \
-       OGREOneStepResampling.sh \
-       OGREPostFreeSurferPipeline.sh \
-       OGREPostFreeSurferPipelineBatch.sh \
-       OGREPreFreeSurferPipeline.sh \
-       OGREPreFreeSurferPipelineBatch.sh \
-       OGRESetUpHCPPipeline.sh \
-       OGRESmoothingProcess.sh \
-       OGRET1w_restore.sh \
-       OGRET2wToT1wReg.sh \
-       OGREmcflirt.sh \
-       OGREmakeregdir.sh)
 
-#for i in ${files[@]};do
-#    cp -p $REPO/HCP/scripts/$i $id/HCP/scripts
-#done
-#START240420
-for i in ${files[@]};do
-    cp -p $repo/lib/$i $id/lib
-done
 
 echo "Installation $id complete." 
